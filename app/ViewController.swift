045212a1d87e8c47e5d3fa414930e590c84b663f
kohei8281 <kohei8281@gmail.com>
2023-12-27 05:04:05 +0900

    Initial xcode settings

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  igit
+//
+//  Created by morinoyu8 on 2023/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
