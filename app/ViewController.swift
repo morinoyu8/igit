395a09e4fcc24a69c6c47247cf68fbed35d2f676
kohei8281 <kohei8281@gmail.com>
2024-01-04 01:11:06 +0900

    Add draw functions

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Draw a line connecting commits
+    func drawLine(start: CGPoint, end: CGPoint, color: UIColor) {
+        let line = Line(frame: self.view.frame, start: start, end: end, color: color, weight: 1.8)
+        self.view.addSubview(line)
+    }
+    
+    // Draw a commit point
+    func drawCommitPoint(point: CGPoint, color: UIColor) {
+        let circle = Circle(frame: self.view.frame, arcCenter: point, radius: 5, color: color)
+        self.view.addSubview(circle)
+    }
+    
+    // Draw a frame for commit information
+    func drawCommitInfoFrame(point: CGPoint, color: UIColor) {
+        let rectangleSize = CGSize(width: 30, height: 30)
+        let roundedRectAngle = RoundedRectangle(frame: self.view.frame, rect: CGRect(origin: point, size: rectangleSize), cornarRadius: 3.0, innerColor: .clear, lineColer: color, lineWiidth: 2.0)
+        self.view.addSubview(roundedRectAngle)
+    }
+    
     // Called when "Select folder" button is pushed
     @IBAction func showDocumentPicker(_ sender: Any) {
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.folder])
