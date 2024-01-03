b811f4619b7c31259308a897c7ebf5941b27a77e
kohei8281 <kohei8281@gmail.com>
2024-01-04 01:59:38 +0900

    Add + operator on CGPoint

---
+++ app/CGPointExt.swift
@@ -0,0 +1,14 @@
+//
+//  CGPointExt.swift
+//  iGit
+//
+//  Created by morinoyu8 on 2024/01/04.
+//
+
+import UIKit
+
+extension CGPoint {
+    static func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+}
