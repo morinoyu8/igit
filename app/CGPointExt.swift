a9abd3044fbe00e1e5bdc4e4958ff1ef77bbecfa
kohei8281 <kohei8281@gmail.com>
2024-01-08 11:33:37 +0900

    Add * and / operators between CGPoint

--- app/CGPointExt.swift
+++ app/CGPointExt.swift
@@ -14,4 +14,10 @@ extension CGPoint {
     static func - (left: CGPoint, right: CGPoint) -> CGPoint {
         return CGPoint(x: left.x - right.x, y: left.y - right.y)
     }
+    static func * (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x * right.x, y: left.y * right.y)
+    }
+    static func / (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x / right.x, y: left.y / right.y)
+    }
 }
