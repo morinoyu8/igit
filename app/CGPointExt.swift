afd8fbfeda71234979ae747a0235195daea64785
kohei8281 <kohei8281@gmail.com>
2024-01-08 11:37:19 +0900

    Add * operators between CGPoint and CGFloat

--- app/CGPointExt.swift
+++ app/CGPointExt.swift
@@ -17,6 +17,12 @@ extension CGPoint {
     static func * (left: CGPoint, right: CGPoint) -> CGPoint {
         return CGPoint(x: left.x * right.x, y: left.y * right.y)
     }
+    static func * (left: CGPoint, right: CGFloat) -> CGPoint {
+        return CGPoint(x: left.x * right, y: left.y * right)
+    }
+    static func * (left: CGFloat, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left * right.x, y: left * right.y)
+    }
     static func / (left: CGPoint, right: CGPoint) -> CGPoint {
         return CGPoint(x: left.x / right.x, y: left.y / right.y)
     }
